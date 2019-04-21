b86d4c08df5788d55580ea68c041a927e3c18bf1
zhanuzakov777@mail.ru <zhanuzakov777@mail.ru>
2019-04-21 21:30:20 +0600

    Adding class Person.java

---
+++ src/main/java/net/proselute/overview/Person.java
@@ -0,0 +1,31 @@
+package net.proselute.overview;
+
+public class Person extends  BaseEntity {
+    private  String firstName;
+    private  String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + super.getId() + '\'' +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
